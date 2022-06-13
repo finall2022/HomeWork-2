6b79ad1d0974ec576053ea9c7960bdce939ca978
finall2022 <finall.git2022@gmail.com>
2022-06-13 22:25:46 +0300

    добавлены задачи домашнего задания ко второму семинару

---
+++ Task 3/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+ */
+Console.Write("Введите число: ");
+string istring = Console.ReadLine();
+int lenght = istring.Length;
+
+Console.WriteLine($"Длина строки = {lenght}");
+int inum = int.Parse(istring);
+int[] arr = new int[lenght - 1];
+//int index = lenght - 1;
+int index = 0;
+while (inum > 0)
+{
+    arr[index] = inum % 10;
+    Console.Write(arr[index]);
+    inum = inum / 10;
+    Console.WriteLine(inum);
+    Console.WriteLine(arr[index]);
+    //lenght = lenght - 1;
+    //index = index - 1;
+}
---
+++ Task 4_2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*  Задача 4: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да 
+7 -> да 
+1 -> нет
+ */
+string numofday(int num)
+{
+    if (num > 0 && num < 7)
+    {
+        //string result = "будний";
+        Console.Write("будний");
+    }
+    else
+    {
+        //string result = "выходной";
+        Console.Write("выходной");
+    }
+    //return result;
+}
+
+Console.Write("Введите порядковый номер дня недели: ");
+string istring = Console.ReadLine();
+int inum = int.Parse(istring);
+numofday(inum);
